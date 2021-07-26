5cce73b3c0fda30125efe03b05fdf7b1925c55cf
Rohit Mamidwar <rohitm95@gmail.com>
2021-07-26 16:33:14 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -5,8 +5,8 @@ let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://rohit-admin:AVCL0NpcgGNrcri@cluster0.yvazp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-  )
+    '<add your mongodb connection string here>'
+  ) // 'mongodb+srv://rohit-admin:AVCL0NpcgGNrcri@cluster0.yvazp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' <----- Please use this format for reference..
     .then(client => {
       console.log('Connected!');
       _db = client.db();
