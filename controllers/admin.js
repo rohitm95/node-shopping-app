7cf7137512c2d26c629f862e192d173f214e9f62
Rohit Mamidwar <rohitm95@gmail.com>
2021-08-07 12:13:35 +0530

    Implemented cookies and sessions

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,12 @@
 const Product = require('../models/product');
+const httpStatus = require('http-status-codes')
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false
+    editing: false,
+    isAuthenticated: req.session.isLoggedIn
   });
 };
 
@@ -46,7 +48,8 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
-        product: product
+        product: product,
+        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -83,7 +86,8 @@ exports.getProducts = (req, res, next) => {
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
-        path: '/admin/products'
+        path: '/admin/products',
+        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
---
+++ controllers/auth.js
@@ -0,0 +1,32 @@
+const User = require('../models/user');
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('610a69a262f84a31146db173')
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        if(err) {
+          console.log(err);
+          res.redirect('/');
+        }
+      })
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect('/');
+  });
+};
